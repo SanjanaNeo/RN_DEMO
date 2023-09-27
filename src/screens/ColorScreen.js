b013467cc046d51199c44af0c24a628d56e3e205
sanjana <sanjana.advani@neosoftmail.com>
2023-09-27 12:52:19 +0530

    Random color generator using flatlist implemented

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native'
+import React, { useState } from 'react'
 
 const ColorScreen = () => {
-  return (
-    <View>
-        <Button title="Add a color"/>
-    </View>
-  )
+    const [colors, setColors] = useState([])
+    console.log("colors", colors);
+
+    const randonRgb = () => {
+        const red = Math.floor(Math.random() * 256)
+        const green = Math.floor(Math.random() * 256)
+        const blue = Math.floor(Math.random() * 256)
+
+        return `rgb(${red},${green},${blue})`
+    }
+    return (
+        <View>
+            <Button
+                title="Add a color"
+                onPress={() => {
+                    setColors([...colors, randonRgb()])
+                }}
+            />
+            <FlatList
+                keyExtractor={(item)=>item}
+                data={colors}
+                renderItem={({ item }) => {
+                    return(
+                        <View style={{ width: 100, height: 100, backgroundColor: item }}></View>
+                    )
+                }}
+            />
+        </View>
+    )
 }
 
 export default ColorScreen
