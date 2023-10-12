09bae3c5addda8ad3e6c8fac83e97dabaa9bdfcd
sanjana <sanjana.advani@neosoftmail.com>
2023-10-12 17:14:58 +0530

    layout exercise included

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
+
+    </View>
+  )
+}
+
+export default BoxScreen
+
+const styles = StyleSheet.create({
+    viewStyle:{
+        borderWidth:3,
+        borderColor:'black',
+        flexDirection:'row',
+        height:200,
+    },
+    textOneStyle:{
+        borderWidth:3,
+        borderColor:'red',
+        flex:4
+    },
+    textTwoStyle:{
+        borderWidth:3,
+        borderColor:'red',
+        flex:4
+    },
+    textThreeStyle:{
+        borderWidth:3,
+        borderColor:'red',
+        flex:2
+    }
+})
